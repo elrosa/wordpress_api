87af19e1cf0d1ac4c121aaf9f204d6571e87c4ee
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-04-23 13:36:45 +0200

    post to REST API

--- lib/wordpress/api/writer.rb
+++ lib/wordpress/api/writer.rb
@@ -3,6 +3,10 @@ module Wordpress
 
     module Writer
 
+      def create_post blog_id, content, options={}
+        path = "/sites/#{blog_id}/posts/new"
+        Mash.from_json(post(path, content.to_json, options))
+      end
     end
 
   end
