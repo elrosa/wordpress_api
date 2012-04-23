a8019490aae9f6ab6d3c21e787cf354f1f795bed
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-04-23 13:46:12 +0200

    oauth2 token change

--- lib/wordpress/helpers/request.rb
+++ lib/wordpress/helpers/request.rb
@@ -10,19 +10,19 @@ module Wordpress
       protected
 
         def get(path, options={})
-          response = access_token.get("#{API_PATH}#{path}", DEFAULT_HEADERS.merge(options))
+          response = access_token.get("#{API_PATH}#{path}", {:headers => DEFAULT_HEADERS.merge(options)})
           raise_errors(response)
           response.body
         end
 
         def post(path, body='', options={})
-          response = access_token.post("#{API_PATH}#{path}", body, DEFAULT_HEADERS.merge(options))
+          response = access_token.post("#{API_PATH}#{path}", {:body => body, :headers => DEFAULT_HEADERS.merge(options)})
           raise_errors(response)
           response
         end
 
         def put(path, body, options={})
-          response = access_token.put("#{API_PATH}#{path}", body, DEFAULT_HEADERS.merge(options))
+          response = access_token.put("#{API_PATH}#{path}", {:body => body, :headers => DEFAULT_HEADERS.merge(options)})
           raise_errors(response)
           response
         end
--- lib/wordpress/version.rb
+++ lib/wordpress/version.rb
@@ -3,7 +3,7 @@ module Wordpress
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 1
-    PATCH = 14
+    PATCH = 15
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
