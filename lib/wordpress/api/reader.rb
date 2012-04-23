a34f41c4ec33ba40c68c1b50320ad544b834ff22
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-04-23 13:15:48 +0200

    wordpress api

--- lib/wordpress/api/reader.rb
+++ lib/wordpress/api/reader.rb
@@ -5,24 +5,8 @@ module Wordpress
 
       def profile(options={})
         path = "/me"
-        simple_query(path, options)
+        Mash.from_json(get path)
       end
-      private
-
-        def simple_query(path, options={})
-
-          if options[:public]
-            path +=":public"
-          elsif fields
-            path +=":(#{fields.map{ |f| f.to_s.gsub("_","-") }.join(',')})"
-          end
-          headers = options[:headers] || {}
-          params_str = ""
-          params = options[:params] || {}
-          params.each { |key, value| params_str << "#{key}=#{value}&"}
-          path += ("?" + params_str.chop) unless (params_str == "")
-          Mash.from_json(get(path, headers))
-        end
 
 
     end
